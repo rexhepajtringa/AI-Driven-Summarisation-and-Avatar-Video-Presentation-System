a695ff73fbadd59ebdd8a37364df2603393f31ed
rexhepajtringa <trexhepaj@york.citycollege.eu>
2024-03-13 23:40:26 +0200

    commit

--- document-handling-service/src/main/resources/static/script.js
+++ document-handling-service/src/main/resources/static/script.js
@@ -1,124 +1,148 @@
 document.addEventListener('DOMContentLoaded', function() {
-    function autoGrowTextArea(textArea) {
-        textArea.style.height = 'auto';
-        textArea.style.height = textArea.scrollHeight + 'px';
-    }
+	function autoGrowTextArea(textArea) {
+		textArea.style.height = 'auto';
+		textArea.style.height = textArea.scrollHeight + 'px';
+	}
 
-    var outputTextArea = document.getElementById('output');
-    outputTextArea.addEventListener('input', function() {
-        autoGrowTextArea(outputTextArea);
-    });
+	var outputTextArea = document.getElementById('output');
+	outputTextArea.addEventListener('input', function() {
+		autoGrowTextArea(outputTextArea);
+	});
 
-    var summaryTextArea = document.getElementById('summaryOutput');
-    summaryTextArea.addEventListener('input', function() {
-        autoGrowTextArea(summaryTextArea);
-    });
+	var summaryTextArea = document.getElementById('summaryOutput');
+	summaryTextArea.addEventListener('input', function() {
+		autoGrowTextArea(summaryTextArea);
+	});
 
-    document.getElementById('uploadBtn').addEventListener('click', function() {
-        var fileInput = document.getElementById('fileInput');
-        
-        if (fileInput.files.length > 0) {
-            sendFile(fileInput.files[0]);
-        } else {
-            alert("Please select a file to upload.");
-        }
-    });
+	document.getElementById('uploadBtn').addEventListener('click', function() {
+		var fileInput = document.getElementById('fileInput');
 
-    function sendFile(file) {
-        var url = "http://localhost:8765/DOCUMENT-HANDLING-SERVICE/api/document/uploadPdf";
-        
-        var formData = new FormData();
-        formData.append("file", file);
+		if (fileInput.files.length > 0) {
+			sendFile(fileInput.files[0]);
+		} else {
+			alert("Please select a file to upload.");
+		}
+	});
 
-        fetch(url, {
-            method: 'POST',
-            body: formData
-        })
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("output").value = data;
-            autoGrowTextArea(document.getElementById("output"));
-        })
-        .catch(error => console.error('Error:', error));
-    }
+	function sendFile(file) {
+		var url = "http://localhost:8765/DOCUMENT-HANDLING-SERVICE/api/document/uploadPdf";
 
-    document.getElementById("summarizeBtn").addEventListener("click", function() {
-        var text = document.getElementById("output").value;
-        var tone = document.getElementById("toneSelect").value;
-        var length = parseInt(document.getElementById("lengthInput").value);
-        var includeReferences = document.getElementById("includeReferences").checked;
-        
-        if (text) {
-            sendTextForSummarization(text, tone, length, includeReferences);
-        } else {
-            alert("Please upload text before summarizing");
-        }
-    });
+		var formData = new FormData();
+		formData.append("file", file);
 
-    function sendTextForSummarization(text, tone, length, includeReferences) {
-        var url = "http://localhost:8765/SUMMARY-SERVICE/api/summarize";
+		fetch(url, {
+			method: 'POST',
+			body: formData
+		})
+			.then(response => response.text())
+			.then(data => {
+				document.getElementById("output").value = data;
+				autoGrowTextArea(document.getElementById("output"));
+			})
+			.catch(error => console.error('Error:', error));
+	}
 
-        var payload = {
-            text: text,
-            tone: tone,
-            length: length,
-            includeReferences: includeReferences
-        };
+	document.getElementById("summarizeBtn").addEventListener("click", function() {
+		var text = document.getElementById("output").value;
+		var tone = document.getElementById("toneSelect").value;
+		var length = parseInt(document.getElementById("lengthInput").value);
+		var includeReferences = document.getElementById("includeReferences").checked;
 
-        fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(payload)
-        })
-        .then(response => {
-            if (response.ok) {
-                return response.text();
-            } else {
-                throw new Error('Server response wasn\'t OK');
-            }
-        })
-        .then(summaryText => {
-            document.getElementById("summaryOutput").value = summaryText;
-            autoGrowTextArea(document.getElementById("summaryOutput"));
-        })
-        .catch(error => console.error('Error:', error));
-    }
+		if (text) {
+			sendTextForSummarization(text, tone, length, includeReferences);
+		} else {
+			alert("Please upload text before summarizing");
+		}
+	});
+
+	function sendTextForSummarization(text, tone, length, includeReferences) {
+		var url = "http://localhost:8765/SUMMARY-SERVICE/api/summarize";
+
+		var payload = {
+			text: text,
+			tone: tone,
+			length: length,
+			includeReferences: includeReferences
+		};
+
+		fetch(url, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(payload)
+		})
+			.then(response => {
+				if (response.ok) {
+					return response.text();
+				} else {
+					throw new Error('Server response wasn\'t OK');
+				}
+			})
+			.then(summaryText => {
+				document.getElementById("summaryOutput").value = summaryText;
+				autoGrowTextArea(document.getElementById("summaryOutput"));
+			})
+			.catch(error => console.error('Error:', error));
+	}
+	
+	
+	
+document.getElementById('speakBtn').addEventListener('click', function() {
+	var text = document.getElementById("summaryOutput").value;
+	if (!text) {
+		alert("Please generate a summary to speak.");
+		return;
+	}
+	sendTextToSpeech(text);
 });
 
-document.getElementById('speakBtn').addEventListener('click', function() {
-    var text = document.getElementById("summaryOutput").value;
-    if (!text) {
-        alert("Please generate a summary to speak.");
-        return;
+
+summaryOutput.addEventListener('input', function() {
+    // Show the 'Speak' button when the text changes
+    document.getElementById('speakBtn').style.display = 'inline-block'; // or 'block', depending on your layout
+    // Hide the audio player container
+    document.getElementById('audioPlayerContainer').style.display = 'none';
+    
+    // Optionally, stop and clear the current audio if it's playing
+    var audioPlayer = document.getElementById('audioPlayer');
+    if (!audioPlayer.paused) {
+        audioPlayer.pause();
+        audioPlayer.currentTime = 0;
     }
-    sendTextToSpeech(text);
+    audioPlayer.src = ''; // Clear the current source
+	autoGrowTextArea(summaryTextArea);
 });
 
 function sendTextToSpeech(text) {
-    var url = "http://localhost:8765/TEXT-TO-VOICE-SERVICE/text-to-speech";
-    var payload = { text: text };
+	var url = "http://localhost:8765/TEXT-TO-VOICE-SERVICE/text-to-speech";
+	var payload = { text: text };
 
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(payload)
+	fetch(url, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(payload)
+	})
+		.then(response => response.blob())
+		.then(blob => {
+        var audioURL = window.URL.createObjectURL(blob);
+        var audioPlayer = document.getElementById('audioPlayer');
+        audioPlayer.src = audioURL;
+        audioPlayer.play();
+        
+        // Hide the Speak button and show the audio player with controls
+        document.getElementById('speakBtn').style.display = 'none';
+        var audioPlayerContainer = document.getElementById('audioPlayerContainer');
+        audioPlayerContainer.style.display = 'block';
     })
-  //  .then(response => response.blob())
-  //  .then(blob => {
-  //      var url = window.URL.createObjectURL(blob);
- //       var audio = new Audio(url);
-  //      audio.play();
-  //  })
-  //  .catch(error => console.error('Error:', error));
-    .then(response => response.text())
-        .then(data => {
-            document.getElementById("output").value = data;
-            autoGrowTextArea(document.getElementById("output"));
-        })
-        .catch(error => console.error('Error:', error));
+    // Add this part to reset the setup if there's an error
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('speakBtn').style.display = 'inline-block';
+        var audioPlayerContainer = document.getElementById('audioPlayerContainer');
+        audioPlayerContainer.style.display = 'none';
+    });
 }
-
+});
--- text-to-voice/app.py
+++ text-to-voice/app.py
@@ -9,9 +9,10 @@ from py_eureka_client import eureka_client
 
 app = Flask(__name__)
 
-eureka_server = "http://localhost:8761"
+eureka_server = "http://host.docker.internal:8761"
 app_name = "text-to-voice-service"
 app_port = 5000
+
 # CORS(app, resources={r"/*": {"origins": "*"}})  # This will enable CORS for all routes under "/api/"
   # This will enable CORS for all routes
 
@@ -19,8 +20,8 @@ app_port = 5000
 eureka_client.init(
     eureka_server=eureka_server,
     app_name=app_name,
-    instance_host='localhost',  # Add this line to explicitly set the host
-    instance_port=app_port
+    instance_port=app_port,
+    instance_host='192.168.2.5',  # The IP address of your machine on the local network
 )
 
 
@@ -29,19 +30,19 @@ def text_to_speech():
     try:
         data = request.json
         text = data.get('text', '')
-        # if not text:
-        #     return jsonify({"error": "No text provided"}), 400
+        if not text:
+            return jsonify({"error": "No text provided"}), 400
 
-        # tts = gTTS(text)
-        # file_name = f"{uuid.uuid4()}.mp3"
-        # file_path = os.path.join("audio_files", file_name)
+        tts = gTTS(text)
+        file_name = f"{uuid.uuid4()}.mp3"
+        file_path = os.path.join("audio_files", file_name)
 
-        # os.makedirs(os.path.dirname(file_path), exist_ok=True)  # Ensure the directory exists
-        # tts.save(file_path)
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)  # Ensure the directory exists
+        tts.save(file_path)
 
-        # # Return the generated audio file
-        # return send_file(file_path, as_attachment=True, download_name=file_name)
-        return jsonify({"text": "Hello World"})
+        # Return the generated audio file
+        return send_file(file_path, as_attachment=True, download_name=file_name)
+        # return jsonify({"text": "It's working"})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
@@ -50,7 +51,7 @@ def text_to_speech():
 if __name__ == '__main__':
     # Starting the Flask app and Eureka client
     # Eureka client is started automatically by py_eureka_client when the Flask app starts.
-    app.run(host='0.0.0.0', port=app_port)
+    app.run(host='0.0.0.0', port=app_port, debug=True)
 
 
 
