0491cb2693e20e55448b7a5adaac17b9763498f0
rexhepajtringa <trexhepaj@york.citycollege.eu>
2024-03-14 20:27:04 +0200

    add adapters

--- text-to-voice/app.py
+++ text-to-voice/app.py
@@ -3,18 +3,18 @@ from gtts import gTTS
 import os
 import uuid
 from py_eureka_client import eureka_client
+from .gttsadapter import GTTSAdapter
+from .elevenlabs_adapter import ElevenLabsAdapter
+from gtts import gTTS
+import os
+import uuid
 # from flask_cors import CORS
-
-
 
 app = Flask(__name__)
 
 eureka_server = "http://host.docker.internal:8761"
 app_name = "text-to-voice-service"
 app_port = 5000
-
-# CORS(app, resources={r"/*": {"origins": "*"}})  # This will enable CORS for all routes under "/api/"
-  # This will enable CORS for all routes
 
 
 eureka_client.init(
@@ -24,6 +24,9 @@ eureka_client.init(
     instance_host='192.168.2.5',  # The IP address of your machine on the local network
 )
 
+# Assuming you're using an environment variable to select the service
+import os
+tts_adapter = GTTSAdapter  # Default to gTTS if not specified
 
 @app.route('/text-to-speech', methods=['POST'])
 def text_to_speech():
@@ -32,17 +35,10 @@ def text_to_speech():
         text = data.get('text', '')
         if not text:
             return jsonify({"error": "No text provided"}), 400
-
-        tts = gTTS(text)
-        file_name = f"{uuid.uuid4()}.mp3"
-        file_path = os.path.join("audio_files", file_name)
-
-        os.makedirs(os.path.dirname(file_path), exist_ok=True)  # Ensure the directory exists
-        tts.save(file_path)
-
-        # Return the generated audio file
-        return send_file(file_path, as_attachment=True, download_name=file_name)
-        # return jsonify({"text": "It's working"})
+        
+        file_path = tts_adapter.synthesize_text(tts_adapter, text)
+        
+        return send_file(file_path, as_attachment=True, download_name=os.path.basename(file_path))
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
@@ -52,7 +48,6 @@ if __name__ == '__main__':
     # Starting the Flask app and Eureka client
     # Eureka client is started automatically by py_eureka_client when the Flask app starts.
     app.run(host='0.0.0.0', port=app_port, debug=True)
-
 
 
 
